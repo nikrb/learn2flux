395066ac90acfc462e161b440ab66e431ceba88d
nik <nik_rb@yahoo.com>
2016-08-08 19:45:02 +0100

    removed article and replaced archives and featured for new setup with todos and favourites. adjusted app to match

--- src/js/app.js
+++ src/js/app.js
@@ -5,16 +5,16 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Bootstrap from 'bootstrap-without-jquery';
 
 import Layout from './pages/Layout';
-import Featured from './pages/Featured';
-import Archives from './pages/Archives';
+import Featured from './pages/Favourites';
+import Archives from './pages/Todos';
 import Settings from './pages/Settings';
 
 const app = document.getElementById('app');
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Layout} >
-      <IndexRoute component={Featured} ></IndexRoute>
-      <Route path="archives(/:article)" name="archives" component={Archives} ></Route>
+      <IndexRoute component={Todos} ></IndexRoute>
+      <Route path="favourites" name="favourites" component={Favourites} ></Route>
       <Route path="settings" name="settings" component={Settings} ></Route>
     </Route>
   </Router>,
