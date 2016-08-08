c6783d5cef951d1326e6fbe46e2cf00cb9833dce
nik <nik_rb@yahoo.com>
2016-08-08 21:26:36 +0100

    create todo actions and add simple button to test todos are created

--- src/js/pages/Todos.js
+++ src/js/pages/Todos.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Todo from "../components/Todo";
+import * as TodoActions from "../actions/TodoActions";
 import TodoStore from "../stores/TodoStore";
 
 export default class Todos extends React.Component {
@@ -18,6 +19,10 @@ export default class Todos extends React.Component {
       });
     });
   }
+  createTodo(){
+    // TODO: add an input field and grab the text
+    TodoActions.createTodo( Date.now());
+  }
   render() {
     const { todos } = this.state;
 
@@ -27,6 +32,9 @@ export default class Todos extends React.Component {
 
     return (
       <div>
+        <button onClick={this.createTodo.bind(this)}>
+          Create!
+        </button>
         <h1>Todos</h1>
         <ul>{TodoComponents}</ul>
       </div>
